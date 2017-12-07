f006c9dcf4c15d9bd2a2d6e1ac2b2cf086713719
Andrei Sukonin <andrei_sukonin@epam.com>
2017-12-07 23:08:05 +0300

    minor fix

--- src/main/java/com/epam/model/Tag.java
+++ src/main/java/com/epam/model/Tag.java
@@ -1,21 +1,30 @@
 package com.epam.model;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import lombok.Data;
+import lombok.ToString;
 
 @Data
 @Entity
 @Table
+@ToString(exclude = "notes")
 public class Tag implements Serializable {
 
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
   private String tag;
+  @ManyToMany(fetch = FetchType.EAGER, mappedBy = "tags", cascade = CascadeType.ALL)
+  private List<Note> notes = new ArrayList<>();
 
 }
--- src/main/java/com/epam/services/impl/NoteService.java
+++ src/main/java/com/epam/services/impl/NoteService.java
@@ -1,12 +1,16 @@
 package com.epam.services.impl;
 
 import com.epam.model.Note;
+import com.epam.model.Tag;
 import com.epam.repository.NoteRepository;
 import com.epam.services.CrudService;
+import java.util.ArrayList;
 import java.util.List;
+import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@Log
 @Service
 public class NoteService implements CrudService<Note, Long> {
 
@@ -17,6 +21,22 @@ public class NoteService implements CrudService<Note, Long> {
     this.noteRepository = noteRepository;
   }
 
+
+  public List<Note> getAllNotesByTag(Tag tag) {
+    List<Note> notes = noteRepository.findAll();
+    List<Note> result = new ArrayList<>();
+    for (Note note : notes) {
+      if (!note.getTags().isEmpty()) {
+        List<Tag> tags = note.getTags();
+        for (Tag tag1 : tags) {
+          if (tag1.getTag().equals(tag.getTag())) {
+            result.add(note);
+          }
+        }
+      }
+    }
+    return result;
+  }
 
   @Override
   public List<Note> getAll() {
